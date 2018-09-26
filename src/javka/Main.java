ecafc1e746fe751b82d36fd2b864e47caedc3145
Jakub Zaboronek <jakub.zaboronek-external@gemalto.com>
2018-09-26 13:39:33 +0200

    Trzeci commit-test

--- src/javka/Main.java
+++ src/javka/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         MessagePrinter messagePrinter=new MessagePrinter();
         messagePrinter.printMessage();
+
     }
 }
--- src/javka/MessageProducerImpl.java
+++ src/javka/MessageProducerImpl.java
@@ -3,6 +3,6 @@ package javka;
 public class MessageProducerImpl implements MessageProducer {
     @Override
     public String getMessage() {
-        return "siemasiema";
+        return "wiadomosc";
     }
 }
